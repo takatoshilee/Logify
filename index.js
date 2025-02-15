25df340f4aae0b00a592af8ab1fa82a814f9954b
Raynard2 <148624014+Raynard2@users.noreply.github.com>
2025-02-15 11:41:40 -0500

    index.js store user input

--- index.js
+++ index.js
@@ -14,3 +14,19 @@ document.getElementById("search-btn").addEventListener("click", function () {
         searchBox.focus();
     }
 });
+
+document.getElementById("search-box").addEventListener("keypress", function(event) {
+    let searchBox = document.getElementById("search-box");
+    
+    if (event.key === "Enter") {
+        let key = event.target.value
+        console.log("Search Query:", key); // Log input value
+        event.target.classList.add("hidden");
+        event.target.value = ""; // Clear input
+        let searchBox = document.getElementById("search-box");
+        searchBox.style.width = "0px"; // Collapse
+        searchBox.style.opacity = "0";
+        setTimeout(() => searchBox.classList.add("hidden"), 300);
+    }
+});
+
